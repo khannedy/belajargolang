e967aedb8baff85666bd8d7b86469bf151afa68b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-08-22 22:01:04 +0700

    tipe data string

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Eko")
+	fmt.Println("Eko" + " " + "Kurniawan")
+
+	fmt.Println("Eko"[0])
+
+	fmt.Println("Kurniawan"[0:4])
+	fmt.Println("Kurniawan"[4:])
+	fmt.Println("Kurniawan"[:5])
+}
