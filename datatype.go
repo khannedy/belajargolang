91ef66da83fc893a57d5db3542479ca009fd371e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-08-22 23:22:33 +0700

    tipe data

---
+++ datatype.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	fmt.Println(1.1)
+
+	fmt.Println("Eko")
+	fmt.Println("Eko Kurniawan")
+
+	fmt.Println(true)
+	fmt.Println(false)
+}
