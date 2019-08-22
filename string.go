8f52ccac108ea071055551fe9d49888e12fb62a9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-08-22 23:41:07 +0700

    manipulasi string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Belajar Go-Lang")
+	fmt.Println("Belajar" + " " + "Go")
+	fmt.Println(len("Belajar"))
+	fmt.Println("Belajar"[0])
+	fmt.Println("Belajar"[1:4])
+	fmt.Println("Belajar"[2:])
+	fmt.Println("Belajar"[:4])
+}
