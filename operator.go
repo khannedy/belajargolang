29b2243500e94d271e6377727de7a756b00caf9c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-08-22 23:30:13 +0700

    operator matematika

---
+++ operator.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 1)
+	fmt.Println(10 - 1)
+	fmt.Println(10 * 2)
+	fmt.Println(10.0 / 3.0)
+	fmt.Println(10 % 3)
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
