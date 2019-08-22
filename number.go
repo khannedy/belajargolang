f977690baa5cb9b8d2677932e4cd327ad6e12923
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-08-22 21:53:35 +0700

    tipe data number

---
+++ number.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1)
+	fmt.Println(1.0)
+
+	fmt.Println(1 + 1)
+	fmt.Println(10 - 2)
+	fmt.Println(10 * 2)
+	fmt.Println(10.0 / 3.0)
+	fmt.Println(10 % 3)
+}
