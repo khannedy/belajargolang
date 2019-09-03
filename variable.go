efc72d55bbed85b5ca97138533270a1ca1f22432
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2019-09-03 21:51:28 +0700

    variable and constant

---
+++ variable.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "Hello World"
+
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+
+	hello = "Hello Golang"
+
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+	fmt.Println(hello)
+
+	var nama string
+
+	fmt.Println(nama)
+
+	nama = "Eko"
+
+	fmt.Println(nama)
+
+	nama_Lengkap := "Eko Kurniawan Khannedy"
+	fmt.Println(nama_Lengkap)
+
+	nilai := 10
+	fmt.Println(nilai)
+
+	const perusahaan string = "Programmer Zaman Now"
+	fmt.Println(perusahaan)
+}
